afe27fb5db303a93bc8fb6ba4437231b2835fbcd
blueplanet <erguolinge@gmail.com>
2016-05-23 14:07:06 +0900

    Add pomotodo

---
+++ main.js
@@ -0,0 +1,27 @@
+// electronモジュールを読み込み
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+// 新しいウィンドウ(Webページ)を生成
+let mainWindow
+function createWindow () {
+  // BrowserWindowインスタンスを生成
+  mainWindow = new BrowserWindow({width: 1280, height: 800})
+  // index.htmlを表示
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  // ウィンドウを閉じたら参照を破棄
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+// アプリの準備が整ったらウィンドウを表示
+app.on('ready', createWindow)
+
+// 全てのウィンドウを閉じたらアプリを終了
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
